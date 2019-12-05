f2a1539f250b7b251fe71d7144d68f8118246404
Vincent Romine <vinjrom@live.com>
2019-12-04 22:42:14 -0600

    First commit

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+
+// Functions open & close navbar
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("navbar").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("navbar").style.marginLeft = "0";
+}
